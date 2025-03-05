28307dae960bebf487c18af54ad5f81ab0a7a960
deciraptor <dethevenaux@gmail.com>
2025-03-05 16:32:06 +0100

    second commit

---
+++ src/main/java/fr/scriptoria/ProjetScriptoriaApplication.java
@@ -0,0 +1,13 @@
+package fr.scriptoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetScriptoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetScriptoriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/scriptoria/ProjetScriptoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.scriptoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetScriptoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
