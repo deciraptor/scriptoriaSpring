4f9ed1ba4a0d842829b6d5a1e7b53efdbb428d4c
deciraptor <dethevenaux@gmail.com>
2025-03-10 14:34:42 +0100

    creation DTO

--- src/main/java/fr/scriptoria/security/dto/AuthResponseDTO.java
+++ src/main/java/fr/scriptoria/security/dto/AuthResponseDTO.java
@@ -4,6 +4,7 @@ public class AuthResponseDTO {
 
     private String accessToken;
     private String tokenType = "Bearer ";
+    
 
     public AuthResponseDTO(String accessToken) {
         this.accessToken = accessToken;
